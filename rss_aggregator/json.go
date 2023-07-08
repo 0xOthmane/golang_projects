924d6534d8b4468ff9a6e146b94f5783f4da57f3
0xOthmane <0xOthmane>
2023-07-08 13:56:59 +0200

    Add error handler

--- rss_aggregator/json.go
+++ rss_aggregator/json.go
@@ -6,6 +6,18 @@ import (
 	"net/http"
 )
 
+func respondWithError(w http.ResponseWriter, code int, msg string)  {
+	if code > 499 {
+		log.Println("Responding with 5XX error:", msg)
+	}
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+	responseWithJSON(w, code, errResponse{
+		Error: msg,
+	})
+}
+
 func responseWithJSON(w http.ResponseWriter, code int, payload interface{})  {
 	data, err := json.Marshal(payload)
 	if err != nil {
@@ -14,7 +26,7 @@ func responseWithJSON(w http.ResponseWriter, code int, payload interface{})  {
 		return
 	}
 	w.Header().Add("Content-Type", "applicationn/json")
-	w.WriteHeader(200)
+	w.WriteHeader(code)
 	w.Write(data)
 
 }
