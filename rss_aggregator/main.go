3d9a645b870f4e0a5e9d788dfe91bd85bb9b2550
0xOthmane <0xOthmane>
2023-07-08 15:27:37 +0200

    Add user handler

--- rss_aggregator/main.go
+++ rss_aggregator/main.go
@@ -1,21 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/0xOthmane/golang_projects/rss_aggregator/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load() // Load .env file
 	portString := os.Getenv("PORT")
+	dbURL := os.Getenv("DB_URL")
 	if portString == "" {
 		log.Fatal("PORT not found")
+	}
+	if dbURL == "" {
+		log.Fatal("DB_URL not found")
+	}
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Unable to connect to the db.")
+	}
+	db := database.New(conn)
+
+	apiCfg := apiConfig{
+		DB: db,
 	}
 	router := chi.NewRouter()
 	router.Use(cors.Handler(cors.Options{
@@ -30,14 +51,15 @@ func main() {
 	// v1Router.HandleFunc("/healthz", handlerReadiness)
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/error", handlerError)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + portString,
 	}
 	log.Printf("Server listening on port %v", portString)
-	err := srv.ListenAndServe()
-	if err != nil {
+	errSrv := srv.ListenAndServe()
+	if errSrv != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("PORT:", portString)
