24a47ac40c16c1f137b68ebb9dbce9c4abf2c1c9
0xOthmane <0xOthmane>
2023-07-08 13:20:47 +0200

    Create a router and server

--- rss_aggregator/README.md
+++ rss_aggregator/README.md
@@ -1,5 +1,19 @@
-go mod init github.com/0xOthmane/golang_projects/rss_project
+
+
+```console
+go mod init github.com/0xOthmane/golang_projects/rss_aggregator
 go get github.com/joho/godotenv
 go mod tidy
 go mod vendor
 go build && ./rss_aggregator
+```
+
+Use of **chi** lightweight, idiomatic and composable router for building Go HTTP services 
+
+```console
+go get github.com/go-chi/chi
+go get github.com/go-chi/cors
+go mod vendor
+go mod tidy
+go mod vendor
+```
--- rss_aggregator/main.go
+++ rss_aggregator/main.go
@@ -3,16 +3,37 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
-func main()  {
-	fmt.Println("Hello world")
-	godotenv.Load()
+func main() {
+	godotenv.Load() // Load .env file
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT not found")
+	}
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+	log.Printf("Server listening on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
 	}
 	fmt.Println("PORT:", portString)
 }
